2e43bc59d895474b1df21c967e31545902628a2c
imoe <lym602@126.com>
2017-08-30 20:26:02 +0300

    changed luym11_docs to luym11

--- conf.py
+++ conf.py
@@ -45,7 +45,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'luym11_docs'
+project = u'luym11'
 copyright = u'2017, yimeng'
 author = u'yimeng'
 
@@ -202,7 +202,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'luym11_docsdoc'
+htmlhelp_basename = 'luym11'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -224,7 +224,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'luym11_docs.tex', u'luym11\\_docs Documentation',
+    (master_doc, 'luym11.tex', u'yimeng\'s Documentation',
      u'yimeng', 'manual'),
 ]
 
@@ -254,7 +254,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'luym11_docs', u'luym11_docs Documentation',
+    (master_doc, 'luym11', u'yimeng\'s Documentation',
      [author], 1)
 ]
 
@@ -268,8 +268,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'luym11_docs', u'luym11_docs Documentation',
-     author, 'luym11_docs', 'One line description of project.',
+    (master_doc, 'luym11', u'yimeng\'s Documentation',
+     author, 'luym11', 'One line description of project.',
      'Miscellaneous'),
 ]
 
